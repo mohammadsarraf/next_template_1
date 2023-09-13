0a3aa7a48505f39bb98725244285e699a342344f
Lilglu4e <mohammadsarrafmstr2000@gmail.com>
2023-09-12 23:16:47 -0400

    cntl + k

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+'use client'
+import React, { useRef, useEffect } from "react";
 import { FaPiedPiperAlt, FaGithub, FaDiscord } from 'react-icons/fa'
 
 export default function Home() {
+	const searchInputRef = useRef<HTMLInputElement | null>(null); // Specify the type as HTMLInputElement | null
+
+	useEffect(() => {
+		const handleKeyPress = (e: KeyboardEvent) => { // Specify the type as KeyboardEvent
+			if (e.ctrlKey && e.key === 'k') {
+				e.preventDefault();
+				if (searchInputRef.current) {
+					searchInputRef.current.focus();
+				}
+			}
+		};
+
+		window.addEventListener('keydown', handleKeyPress);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeyPress);
+		};
+	}, []);
+
 	return (
 		<nav className="flex w-screen h-20 py-3 px-10 border-b border-gray-700 justify-between">
 			<div>
@@ -12,6 +32,7 @@ export default function Home() {
 				<div>Contact ↗</div>
 				<div className="relative">
 					<input
+						ref={searchInputRef}
 						type="text"
 						className="bg-gray-900 h-10 px-5 pr-10 rounded-md text-sm focus:outline-none w-64 text-white"
 						placeholder="Search documentation..."
@@ -22,5 +43,5 @@ export default function Home() {
 				<FaDiscord className={`text-white h-10 w-10`} />
 			</div>
 		</nav>
-	)
+	);
 }
