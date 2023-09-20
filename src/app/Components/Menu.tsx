94cdd44e2ff01e3e28d11be9d004f3cf8d28f842
Lilglu4e <mohammadsarrafmstr2000@gmail.com>
2023-09-19 22:42:28 -0400

    drag update

--- src/app/Components/Menu.tsx
+++ src/app/Components/Menu.tsx
@@ -1,172 +1,206 @@
-import React, { useState } from "react";
+
+
+import React, { useState, useEffect, useRef } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import { FaPencilAlt } from "react-icons/fa"; // Import the Pencil icon
+import { RxHamburgerMenu } from 'react-icons/rx'
 
 interface Button {
-  Topic: string;
-  Children: { key: string; value: string }[];
-  key: string;
+	Topic: string;
+	Children: { key: string; value: string }[];
+	key: string;
 }
 
 const initialButtons: Button[] = [
-  {
-    Topic: "Getting Started",
-    key: "Introduction",
-    Children: [
-      { key: "1", value: "Installation" },
-      { key: "2", value: "Editor Setup" },
-      { key: "3", value: "Using with Preprocessors" },
-      { key: "4", value: "Optimizing for Production" },
-      { key: "5", value: "Browser Support" },
-      { key: "6", value: "Upgrade Guide" },
-    ],
-  },
-  {
-    Topic: "Core Concepts",
-    key: "Core Concepts",
-    Children: [
-      { key: "1", value: "Utility-First Fundamentals" },
-      { key: "2", value: "Handling Hover, Focus, and Other States" },
-      { key: "3", value: "Responsive Design" },
-      { key: "4", value: "Dark Mode" },
-      { key: "5", value: "Adding Custom Styles" },
-      { key: "6", value: "Functions & Directives" },
-    ],
-  },
+	{
+		Topic: "Getting Started",
+		key: "Introduction",
+		Children: [
+			{ key: "1", value: "Installation" },
+			{ key: "2", value: "Editor Setup" },
+			{ key: "3", value: "Using with Preprocessors" },
+			{ key: "4", value: "Optimizing for Production" },
+			{ key: "5", value: "Browser Support" },
+			{ key: "6", value: "Upgrade Guide" },
+		],
+	},
+	{
+		Topic: "Core Concepts",
+		key: "Core Concepts",
+		Children: [
+			{ key: "1", value: "Utility-First Fundamentals" },
+			{ key: "2", value: "Handling Hover, Focus, and Other States" },
+			{ key: "3", value: "Responsive Design" },
+			{ key: "4", value: "Dark Mode" },
+			{ key: "5", value: "Adding Custom Styles" },
+			{ key: "6", value: "Functions & Directives" },
+		],
+	},
+	{
+		Topic: "Customization",
+		key: "Customization",
+		Children: [
+			{ key: "1", value: "Configuration" },
+			{ key: "2", value: "Content" },
+			{ key: "3", value: "Theme" },
+			{ key: "4", value: "Screens" },
+			{ key: "5", value: "Colors" },
+			{ key: "6", value: "Spacing" },
+			{ key: "7", value: "Plugins" },
+			{ key: "8", value: "Presets" },
+		],
+	},
 ];
 
 export default function Menu() {
-  const [buttons, setButtons] = useState<Button[]>(initialButtons);
-  const [selectedButton, setSelectedButton] = useState<string | null>(null);
-  const [selectedChild, setSelectedChild] = useState<string | null>(null);
-  const [isThemeDropdownOpen, setIsThemeDropdownOpen] = useState(false);
-  const [selectedTheme, setSelectedTheme] = useState("light");
-  const [isEditing, setIsEditing] = useState(false);
-  const [changesMade, setChangesMade] = useState(false);
+	const [buttons, setButtons] = useState<Button[]>(initialButtons);
+	const [selectedButton, setSelectedButton] = useState<string | null>(null);
+	const [selectedChild, setSelectedChild] = useState<string | null>(null);
+	const [isThemeDropdownOpen, setIsThemeDropdownOpen] = useState(false);
+	const [selectedTheme, setSelectedTheme] = useState("light");
+	const [isEditing, setIsEditing] = useState(false);
+	const [changesMade, setChangesMade] = useState(false);
+	const searchInputRef = useRef<HTMLInputElement | null>(null);
+	useEffect(() => {
+		const handleKeyPress = (e: KeyboardEvent) => {
+			if (e.ctrlKey && e.key === 'k') {
+				e.preventDefault();
+				if (searchInputRef.current) {
+					searchInputRef.current.focus();
+				}
+			}
+		};
 
-  const handleButtonClick = (buttonName: string, childKey: string | null) => {
-    setSelectedButton(buttonName);
-    setSelectedChild(childKey);
-  };
+		window.addEventListener('keydown', handleKeyPress);
 
-  const toggleThemeDropdown = () => {
-    setIsThemeDropdownOpen(!isThemeDropdownOpen);
-  };
+		return () => {
+			window.removeEventListener('keydown', handleKeyPress);
+		};
+	}, []);
+	const handleButtonClick = (buttonName: string, childKey: string | null) => {
+		setSelectedButton(buttonName);
+		setSelectedChild(childKey);
+	};
 
-  const handleThemeChange = (theme: string) => {
-    setSelectedTheme(theme);
-    toggleThemeDropdown();
-  };
+	const toggleThemeDropdown = () => {
+		setIsThemeDropdownOpen(!isThemeDropdownOpen);
+	};
 
-  const toggleEditing = () => {
-    setIsEditing(!isEditing);
-    setChangesMade(false);
-  };
+	const handleThemeChange = (theme: string) => {
+		setSelectedTheme(theme);
+		toggleThemeDropdown();
+	};
 
-  const onDragEnd = (result: any) => {
-    if (!result.destination) {
-      return;
-    }
+	const toggleEditing = () => {
+		setIsEditing(!isEditing);
+		setChangesMade(false);
+	};
 
-    const { source, destination } = result;
-    const updatedButtons = [...buttons];
+	const onDragEnd = (result: any) => {
+		if (!result.destination) {
+			return;
+		}
 
-    if (source.droppableId === "buttons" && destination.droppableId === "buttons") {
-      const sourceButtonIndex = source.index;
-      const destButtonIndex = destination.index;
+		const { source, destination } = result;
+		const updatedButtons = [...buttons];
 
-      const [movedButton] = updatedButtons.splice(sourceButtonIndex, 1);
-      updatedButtons.splice(destButtonIndex, 0, movedButton);
-      setChangesMade(true);
-    }
+		if (source.droppableId === "buttons" && destination.droppableId === "buttons") {
+			const sourceButtonIndex = source.index;
+			const destButtonIndex = destination.index;
 
-    setButtons(updatedButtons);
-  };
+			const [movedButton] = updatedButtons.splice(sourceButtonIndex, 1);
+			updatedButtons.splice(destButtonIndex, 0, movedButton);
+			setChangesMade(true);
+		}
 
-  const handleDoneClick = () => {
-    setChangesMade(false);
-    setIsEditing(false);
-  };
+		setButtons(updatedButtons);
+	};
 
-  return (
-    <div className="flex flex-col flex-1 px-10 py-3 relative h-full">
-      <div className="relative my-8">
-        <input
-          type="text"
-          className="bg-gray-800 text-white placeholder-gray-400 h-10 px-4 py-2 pr-10 rounded-md text-sm focus:outline-none w-64"
-          placeholder="Search documentation..."
-        />
-        <span className="absolute right-5 top-1/2 transform -translate-y-1/2 text-gray-400 text-xs">
-          {`CNTL K`}
-        </span>
-      </div>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="buttons" isDropDisabled={!isEditing}>
-          {(provided) => (
-            <div ref={provided.innerRef} {...provided.droppableProps}>
-              {buttons.map((button, index) => (
-                <Draggable
-                  key={button.key}
-                  draggableId={button.key}
-                  index={index}
-                  isDragDisabled={!isEditing}
-                >
-                  {(provided, snapshot) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      className={`flex flex-col my-3 ${snapshot.isDragging ? "opacity-50" : ""}`}
-                    >
-                      <div className="flex items-center">
-                        <div
-                          {...provided.dragHandleProps}
-                          className={`text-blue-600 cursor-pointer ${isEditing ? "" : "hidden"}`}
-                        >
-                          <FaPencilAlt /> {/* Use the Pencil icon */}
-                        </div>
-                        <p
-                          className={`text-white text-xs font-bold ml-2 ${selectedButton === button.key ? "text-blue-600" : ""}`}
-                          onClick={() => handleButtonClick(button.key, null)}
-                        >
-                          {button.Topic}
-                        </p>
-                      </div>
+	const handleDoneClick = () => {
+		setChangesMade(false);
+		setIsEditing(false);
+	};
 
-                      <div className="flex flex-col my-2">
-                        {button.Children.map((child) => (
-                          <button
-                            key={child.key}
-                            className={`text-xs text-bold whitespace-nowrap text-left p-2 pl-4 border-l rounded-e-md hover:border-l-gray-600 ${selectedButton === button.key && selectedChild === child.key ? "text-blue-600 border-l-blue-600" : "text-gray-300"}`}
-                            onClick={() => handleButtonClick(button.key, child.key)}
-                          >
-                            {child.value}
-                          </button>
-                        ))}
-                      </div>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-      {isEditing ? (
-        <button
-          className={`bg-green-600 text-white px-2 py-1 rounded-md mt-4`}
-          onClick={handleDoneClick}
-        >
-          Done
-        </button>
-      ) : (
-        <button
-          className={`bg-blue-600 text-white px-2 py-1 rounded-md mt-4 ${isEditing ? "hidden" : ""}`}
-          onClick={toggleEditing}
-        >
-          Edit
-        </button>
-      )}
-    </div>
-  );
+	return (
+		<div className="flex flex-col flex-1 px-10 py-3 relative h-full">
+			<div className="relative my-8">
+				<input
+					ref={searchInputRef}
+					type="text"
+					className="bg-gray-800 text-white placeholder-gray-400 h-10 px-4 py-2 pr-10 rounded-md text-sm focus:outline-none w-64"
+					placeholder="Search documentation..."
+				/>
+				<span className="absolute right-8 top-1/2 transform -translate-y-1/2 text-gray-400 text-xs">
+					{`CNTL K`}
+				</span>
+			</div>
+			<DragDropContext onDragEnd={onDragEnd}>
+				<Droppable droppableId="buttons" isDropDisabled={!isEditing}>
+					{(provided) => (
+						<div className={`overflow-auto pr-3`} ref={provided.innerRef} {...provided.droppableProps}>
+							{buttons.map((button, index) => (
+								<Draggable
+									key={button.key}
+									draggableId={button.key}
+									index={index}
+									isDragDisabled={!isEditing}
+								>
+									{(provided, snapshot) => (
+										<div
+											ref={provided.innerRef}
+											{...provided.draggableProps}
+											className={`flex flex-col my-3 ${snapshot.isDragging ? "opacity-50" : ""}`}
+										>
+											<div className="flex items-center justify-between">
+
+												<p
+													className={`text-white text-xs font-bold ${selectedButton === button.key ? "text-blue-600" : ""}`}
+													onClick={() => handleButtonClick(button.key, null)}
+												>
+													{button.Topic}
+												</p>
+												<div
+													{...provided.dragHandleProps}
+													className={`text-gray-500 cursor-pointer ${isEditing ? "" : "hidden"}`}
+												>
+													<RxHamburgerMenu />
+												</div>
+											</div>
+
+											<div className="flex flex-col my-2">
+												{button.Children.map((child) => (
+													<button
+														key={child.key}
+														className={`text-xs text-bold whitespace-nowrap text-left p-2 pl-4 border-l rounded-e-md hover:border-l-gray-600 ${selectedButton === button.key && selectedChild === child.key ? "text-blue-600 border-l-blue-600" : "text-gray-300"}`}
+														onClick={() => handleButtonClick(button.key, child.key)}
+													>
+														{child.value}
+													</button>
+												))}
+											</div>
+										</div>
+									)}
+								</Draggable>
+							))}
+							{provided.placeholder}
+						</div>
+					)}
+				</Droppable>
+			</DragDropContext>
+			{isEditing ? (
+				<button
+					className={`bg-green-600 text-white px-2 py-1 rounded-md mt-4`}
+					onClick={handleDoneClick}
+				>
+					Done
+				</button>
+			) : (
+				<button
+					className={`bg-blue-600 text-white px-2 py-1 rounded-md mt-4 ${isEditing ? "hidden" : ""}`}
+					onClick={toggleEditing}
+				>
+					Edit
+				</button>
+			)}
+		</div>
+	);
 }
--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -34,18 +34,6 @@ export default function Navbar() {
         <div className="text-white hover:underline cursor-pointer">About</div>
         <div className="text-white hover:underline cursor-pointer">Contact ↗</div>
 
-        <div className="relative">
-          <input
-            ref={searchInputRef}
-            type="text"
-            className="bg-gray-800 text-white placeholder-gray-400 h-10 px-4 py-2 pr-10 rounded-md text-sm focus:outline-none w-64"
-            placeholder="Search documentation..."
-          />
-          <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 text-xs">
-            {/* <FaSearch className="text-lg" /> */}{`CNTL K`}
-          </span>
-        </div>
-
         <a
           href="https://github.com/your-github-repo"
           target="_blank"
