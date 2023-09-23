857e2bfbd9387708c731b1d98563e83a8cf2740a
Lilglu4e <mohammadsarrafmstr2000@gmail.com>
2023-09-23 09:52:06 -0400

    small fix

--- src/app/docs/[slug]/page.tsx
+++ src/app/docs/[slug]/page.tsx
@@ -16,20 +16,19 @@ function getContentByChildName(childName: string) {
     // Loop through the jsonData to find the child with the matching name
     for (const topic of jsonData) {
         for (const child of topic.Children) {
-            if (child.value === childName) {
+            // console.log(child)
+            if (child.key === childName) {
                 // Return the content of the matching child
                 return child.content;
             }
         }
     }
-
-    // Return a message if the child name is not found
-    return 'Child not found';
+    return "Child not found"
 }
-
 export default function docPage({ params }: Props) {
     return (
         <>
+            {console.log(params.slug)}
             <div className="flex flex-col h-screen ">
                 <Navbar />
                 <ChildPage
